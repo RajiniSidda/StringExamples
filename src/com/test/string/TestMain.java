65d2e1f600db14b12aedacdd95be0756b72f58e9
RajiniSidda <41594129+RajiniSidda@users.noreply.github.com>
2018-07-23 16:27:12 -0500

    This is a string example

---
+++ src/com/test/string/TestMain.java
@@ -0,0 +1,12 @@
+package com.test.string;
+
+/**
+ * Created by bhara on 7/23/2018.
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println("This is a String example");
+
+    }
+}
