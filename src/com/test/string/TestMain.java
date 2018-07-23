f2444be218294aa161f1111ba503aa16068143cb
RajiniSidda <41594129+RajiniSidda@users.noreply.github.com>
2018-07-23 16:43:18 -0500

    temp/story/string-equality

--- src/com/test/string/TestMain.java
+++ src/com/test/string/TestMain.java
@@ -8,5 +8,13 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("This is a String example");
 
+        String a = new String("Hello");
+        String b = "hello";
+        String c = new String("hello");
+
+        System.out.println("a eqs b = "+a.equals(b));
+        System.out.println("a eqs c = "+a.equals(c));
+        System.out.println("b eqs c = "+b.equals(c));
+
     }
 }
