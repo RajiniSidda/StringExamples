d2acbb2db0f80896cf88ed4f1fca17a126ad659f
RajiniSidda <41594129+RajiniSidda@users.noreply.github.com>
2018-07-23 22:48:50 -0500

    String Concat Eg

---
+++ src/ConcatEg.java
@@ -0,0 +1,11 @@
+/**
+ * Created by bhara on 7/23/2018.
+ */
+public class ConcatEg {
+    public static void main(String[] args) {
+
+String S="Geeth";
+String S1="Athreya";
+        System.out.println(S+S1);
+    }
+}
